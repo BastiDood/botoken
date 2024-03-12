978e6860a9cffa5931ff9e4938ecbebd2fe25feb
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-03-12 18:28:00 +0800

    chore: run formatter

--- sveltekit/src/lib/assert.ts
+++ sveltekit/src/lib/assert.ts
@@ -1,4 +1,4 @@
-export class AssertionError extends Error { }
+export class AssertionError extends Error {}
 
 export function assert(condition: unknown, msg: string): asserts condition {
     if (!condition) throw new AssertionError(msg);
