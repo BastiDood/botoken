e9059f0ec7ddafa3bb66697b0e4a0ed5d92a688d
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-03-13 14:31:15 +0800

    fix(hardhat): deploy with correct arguments

--- hardhat/scripts/deploy.ts
+++ hardhat/scripts/deploy.ts
@@ -1,7 +1,7 @@
 import { ethers } from 'hardhat';
 
 async function main() {
-    const contract = await ethers.deployContract('Botoken');
+    const contract = await ethers.deployContract('Botoken', [1000n]);
     const { target } = await contract.waitForDeployment();
     console.log(target);
 }
