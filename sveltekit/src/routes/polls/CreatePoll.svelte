7c1fc81699280505291e0d979449d8b08ab49be9
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-03-15 06:30:45 +0800

    fix(app): redirect to the correct poll

--- sveltekit/src/routes/polls/CreatePoll.svelte
+++ sveltekit/src/routes/polls/CreatePoll.svelte
@@ -40,7 +40,7 @@
 
             const [address, ..._rest] = event.args;
             assert(isAddress(address), 'non-address event argument for poll creation');
-            await goto(`/poll/${address}`);
+            await goto(`/polls/${address}`);
         } catch (err) {
             if (isError(err, 'CALL_EXCEPTION') || isError(err, 'ACTION_REJECTED')) {
                 const reason = err.reason ?? 'unknown reason';
