0e44854bcab58011ac345efd3ca3a56386b6251f
Basti Ortiz <39114273+BastiDood@users.noreply.github.com>
2024-03-15 13:25:00 +0800

    feat(app): automatically refresh poll status

--- sveltekit/src/routes/polls/GetPoll.svelte
+++ sveltekit/src/routes/polls/GetPoll.svelte
@@ -23,50 +23,57 @@
         ]);
         return { title, pot, balance };
     }
+
+    let display = Symbol();
+    let history = display;
 </script>
 
 <div class="space-y-4">
-    {#await retrieve(poll)}
-        <ProgressBar />
-    {:then result}
-        <DisplayVote {...result} />
-    {:catch err}
-        <ErrorAlert>{err}</ErrorAlert>
-    {/await}
+    {#key display}
+        {#await retrieve(poll)}
+            <ProgressBar />
+        {:then result}
+            <DisplayVote {...result} />
+        {:catch err}
+            <ErrorAlert>{err}</ErrorAlert>
+        {/await}
+    {/key}
     <hr />
-    <CastVote {poll} {user} />
+    <CastVote {poll} {user} on:close={() => (display = history = Symbol())} on:vote={() => (display = Symbol())} />
     <hr />
-    {#await user.queryFilter(closeEvents, block - 8, block)}
-        <ProgressBar />
-    {:then events}
-        <section class="prose dark:prose-invert max-w-none">
-            <h2 class="h2">Past Poll Results</h2>
-            {#if events.length === 0}
-                <WarningAlert>There have been no past events.</WarningAlert>
-            {:else}
-                <div class="card p-2">
-                    <dl class="list-dl">
-                        {#each events as { args: [_address, title, pot, balance] }}
-                            {@const bal = balance > 0 ? `+${balance}` : balance.toString()}
-                            <div>
-                                {#if balance > 0}
-                                    <span class="badge bg-success-500">+{balance}</span>
-                                {:else if balance < 0}
-                                    <span class="badge bg-error-500">{balance}</span>
-                                {:else}
-                                    <span class="badge bg-warning-500">0</span>
-                                {/if}
-                                <span class="flex-auto">
-                                    <dt class="font-bold">[{pot} BTK] {title}</dt>
-                                    <dd class="text-sm opacity-50">The consensus is {bal}.</dd>
-                                </span>
-                            </div>
-                        {/each}
-                    </dl>
-                </div>
-            {/if}
-        </section>
-    {:catch err}
-        <ErrorAlert>{err}</ErrorAlert>
-    {/await}
+    {#key history}
+        {#await user.queryFilter(closeEvents, block - 8, block)}
+            <ProgressBar />
+        {:then events}
+            <section class="prose dark:prose-invert max-w-none">
+                <h2 class="h2">Past Poll Results</h2>
+                {#if events.length === 0}
+                    <WarningAlert>There have been no past events.</WarningAlert>
+                {:else}
+                    <div class="card p-2">
+                        <dl class="list-dl">
+                            {#each events as { args: [_address, title, pot, balance] }}
+                                {@const bal = balance > 0 ? `+${balance}` : balance.toString()}
+                                <div>
+                                    {#if balance > 0}
+                                        <span class="badge bg-success-500">+{balance}</span>
+                                    {:else if balance < 0}
+                                        <span class="badge bg-error-500">{balance}</span>
+                                    {:else}
+                                        <span class="badge bg-warning-500">0</span>
+                                    {/if}
+                                    <span class="flex-auto">
+                                        <dt class="font-bold">[{pot} BTK] {title}</dt>
+                                        <dd class="text-sm opacity-50">The consensus is {bal}.</dd>
+                                    </span>
+                                </div>
+                            {/each}
+                        </dl>
+                    </div>
+                {/if}
+            </section>
+        {:catch err}
+            <ErrorAlert>{err}</ErrorAlert>
+        {/await}
+    {/key}
 </div>
